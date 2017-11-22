09b05d1069f986f29896c1d09983624785abd62d
Yasin Kaymaz <yasinkaymaz@github.com>
2017-11-22 16:46:37 -0500

    now using python/2.7.9

--- testFolder/EBVtypingSensitivity.sh
+++ testFolder/EBVtypingSensitivity.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 toolDir='/home/yk42w/codes/ViralGenomeAssembly'
+module unload python/2.7.5
 module load python/2.7.9
 module load python/2.7.9_packages/pandas/0.17.1
 for genome in `grep ">" sequences.aln.fasta|grep -v NC_|sed 's/>//g'`;
 do
   echo $genome;
-  $toolDir/bin/MSA_parser_cleaner.py $genome NC_007605 sequences.aln.fasta 1;
-  $toolDir/bin/MSA_parser_cleaner.py $genome NC_009334 sequences.aln.fasta 2;
+  python $toolDir/bin/MSA_parser_cleaner.py $genome NC_007605 sequences.aln.fasta 1;
+  python $toolDir/bin/MSA_parser_cleaner.py $genome NC_009334 sequences.aln.fasta 2;
 done
