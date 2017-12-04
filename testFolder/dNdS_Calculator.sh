4bbdca7da0d6701f275514a14d42b64f486855ff
Yasin Kaymaz <yasinkaymaz@github.com>
2017-12-04 11:49:22 -0500

    added a filtration step to the script for samples that are not covered more than %50 of the Ref gene.

--- testFolder/dNdS_Calculator.sh
+++ testFolder/dNdS_Calculator.sh
@@ -14,7 +14,10 @@ do
   echo $gene;
   python $toolDir/bin/MSA_gene_extractor.py $AlignmentFile 1 $RefGenesDir/"$gene".bed;
   perl $toolDir/bin/fasta_to_tab.pl "$gene".bed.aln.fasta > "$gene".aln.tab;
-  perl $toolDir/bin/SNAP.pl "$gene".aln.tab;
+  RefORFlen=`grep NC_007605 "$gene".aln.tab |sed 's/[n|-]//g'|awk '{print length($2)}'`;
+  #Exclude genome samples if the given gene is not covered more than %50:
+  awk -v RefORFlen="$RefORFlen" '{seq=$2;gsub(/n|-/,"",seq); if ( 100*length(seq)/RefORFlen > 50) print}' "$gene".aln.tab > "$gene".aln.filtered.tab
+  perl $toolDir/bin/SNAP.pl "$gene".aln.filtered.tab;
   grep all summary.* |awk -v gene="$gene" '{print gene"\t"$0 }' >> EBV_Genes_dNdS_summary_"${AlignmentFile%.aln.fasta}".txt;
   rm summary.*;
 done
