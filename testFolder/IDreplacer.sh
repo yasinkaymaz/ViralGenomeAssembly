6a327aed64edb4dd11bfd7747adecc8e31e1b49a
Yasin Kaymaz <yasinkaymaz@github.com>
2018-01-11 10:34:29 -0500

    a bulk push plots in the paper.

--- testFolder/IDreplacer.sh
+++ testFolder/IDreplacer.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+InputFasta=$1
 
-awk 'NR==FNR {a[$1]=$2;next} {for ( i in a) gsub(i,a[i])}1' EBV_SequencingSet_ID-Key.txt \
-227.Genomes.aln.fasta > 227.Genomes.aln_IDchanged.fasta
+toolDir='/Users/yasinkaymaz/Dropbox/codes/ViralGenomeAssembly'
+
+
+
+$toolDir/bin/fasta_to_tab.pl $InputFasta |\
+awk 'FNR==NR{a[$1]=$2;next}{print ">"a[$1],"\n",$2}' $toolDir/PapeRCodes/EBV_SequencingSet_ID-Key3.txt - \
+> "${InputFasta%.fasta}".IDreplaced.fasta
+
+
+#Alternative
+#awk 'NR==FNR {a[$1]=$2;next} {for ( i in a) gsub(i,a[i])}1' EBV_SequencingSet_ID-Key.txt \
+#227.Genomes.aln.fasta > 227.Genomes.aln_IDchanged.fasta
 #Code from https://www.unix.com/unix-for-dummies-questions-and-answers/143054-best-method-replacing-multiple-strings-multiple-files-sed-awk-most-simple-preferred.html
 #awk 'NR==FNR {a[$1]=$4;next} {for ( i in a) gsub(i,a[i])}1' key.labeled3.txt dnds2/LMP1.aln.filtered.rc.tab|awk '{print ">"$1"\n"$2}' > geneFastas/LMP1.aln.filtered.fasta
 # for gene in BYRF1.aln.filtered EBNA3A.aln.filtered EBNA3B.aln.filtered EBNA3C.aln.filtered ;
