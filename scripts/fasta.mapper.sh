2331a6e36a1b3a9fc7d50d5068443485f47084a5
Yasin Kaymaz <yasinkaymaz@github.com>
2019-03-24 14:59:20 -0400

    changed paths

--- scripts/fasta.mapper.sh
+++ scripts/fasta.mapper.sh
@@ -16,6 +16,8 @@ PICARDPATH="$toolDir/bin/linux/picard-2.14.1/build/libs"
 Bowtie2PATH="$toolDir/bin/linux/bowtie2-2.3.3.1-linux-x86_64"
 nt=2
 
+export PATH="$Bowtie2PATH":"$toolDir":"$toolDir/bin/":"$PICARDPATH":$PATH
+
 #EBV genome type
 if [ "$type" = "1" ]
 then
@@ -42,10 +44,10 @@ seqnum=`grep ">" $InputGenomeFasta |wc -l`;
 for ((i=1;i<=$seqnum;i++));
   do
     let x=$i*2;
-    head -$x $InputGenomeFasta | tail -2 | perl ~/codes/ViralGenomeAssembly/bin/fasta_chop.pl;
+    head -$x $InputGenomeFasta | tail -2 | perl fasta_chop.pl;
 done > "${InputGenomeFasta%.fa}"_contigs_chopped.fa
 
-$Bowtie2PATH/bowtie2 -p $nt -f -x $toolDir/resources/Bowtie2Index/$EBVgenome \
+bowtie2 -p $nt -f -x $toolDir/resources/Bowtie2Index/$EBVgenome \
 -U "${InputGenomeFasta%.fa}"_contigs_chopped.fa \
 -S "${InputGenomeFasta%.fa}"_mapped2_"$Type".sam
 
@@ -53,14 +55,14 @@ $Bowtie2PATH/bowtie2 -p $nt -f -x $toolDir/resources/Bowtie2Index/$EBVgenome \
 
 #Sort sam file and output as bam
 java -Xmx10g -XX:ParallelGCThreads=$nt -jar \
-$PICARDPATH/picard.jar \
+picard.jar \
 SortSam \
 INPUT="${InputGenomeFasta%.fa}"_mapped2_"$Type".sam \
 OUTPUT="${InputGenomeFasta%.fa}"_mapped2_"$Type"_sorted.bam \
 SORT_ORDER=coordinate
 #Index bam file
 java -Xmx10g -XX:ParallelGCThreads=$nt -jar \
-$PICARDPATH/picard.jar \
+picard.jar \
 BuildBamIndex \
 INPUT="${InputGenomeFasta%.fa}"_mapped2_"$Type"_sorted.bam
 
